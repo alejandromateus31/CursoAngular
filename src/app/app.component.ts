d862451d7233d31cbbd463abdff896b831e6eed9
alejandromateus31 <35266240+alejandromateus31@users.noreply.github.com>
2023-02-28 15:18:29 -0500

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEnd';
+ nombre = "Diego"
+ textoPlaceHolder = 'Escriba algo aqui';
+ deshabilitado = true;
+ Seturl ='https://uploads.toptal.io/blog/image/125413/toptal-blog-image-1518523133236-d2bc894552c77f954f1e5ce48da6604d.png';
+ texto = 'Esto es un video sobre event binding';
+
+constructor() {
+  setInterval( () => this.deshabilitado = false , 5000  )
+
+}
+
+getSuma(numero1 : number , numero2 : number){
+  return numero1 + numero2;
+}
+
+cambiarTexto() : void{
+  this.texto = 'En el proximo video vamos a ver two way data-binding';
+
+}
+
+
 }
